898f3d5fdcee80dbae027e7e06235a002999d921
aciolekwaw <abialkowska247@gmail.com>
2015-10-29 13:50:27 +0100

    getMinutes

--- app/src/main/java/com/example/aciolekwaw/notificationtimer/MainActivity.java
+++ app/src/main/java/com/example/aciolekwaw/notificationtimer/MainActivity.java
@@ -13,8 +13,8 @@ import android.widget.Button;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
-import org.joda.time.DateTime;
-import org.joda.time.Seconds;
+import org.joda.time.LocalTime;
+import org.joda.time.Period;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,21 +35,26 @@ public class MainActivity extends AppCompatActivity {
     TextView textTime;
     final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("HH:mm");
     Button buttonTimerNotification;
+    TextView textViewDifference;
+    TextView tv;
+    private Object zone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final TextView textViewDifference = (TextView) findViewById(R.id.textViewDifference);
 
         notificationManager = (NotificationManager) getSystemService(Context
                 .NOTIFICATION_SERVICE);
         setContentView(R.layout.activity_main);
         setTitle("LollipopNotifications");
 
+        textViewDifference  = (TextView) findViewById(R.id.textViewDifference);
+        tv = (TextView) findViewById(R.id.textViewTime1);
+
+
         textTime = (TextView) findViewById(R.id.textViewTime);
         textTime.setText(_sdfWatchTime.format(new Date()));
-
 
 
         headsUpButton = (Button) findViewById(R.id.buttonHeadsUp);
@@ -59,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
                 createHeadsUpNotification();
             }
         });
-
 
         normalButton = (Button) findViewById(R.id.normalButton);
         normalButton.setOnClickListener(new View.OnClickListener() {
@@ -79,12 +83,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 createTimeNotification();
-                textViewDifference.setText(timeToEnd());
-
+                tv.setText(timeToEnd());
             }
         });
-
-
     }
 
     @Override
@@ -107,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_launcher_notification)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setVibrate(new long[] {1, 1, 1})
+                .setVibrate(new long[]{1, 1, 1})
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentTitle("Simple Heads-Up Notification")
                 .setContentText("This is a normal notification.");
@@ -123,7 +124,9 @@ public class MainActivity extends AppCompatActivity {
                 .setVibrate(new long[]{1, 1, 1})
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentTitle("Time head up notification")
-                .setContentText(textTime.getText().toString());
+                .setContentText(textTime.getText().toString())
+                .setContentText(timeToEnd());
+
 
         notificationManager.notify(HEADS_UP_NOTIFICATION_ID, notificationBuilder.build());
     }
@@ -141,79 +144,26 @@ public class MainActivity extends AppCompatActivity {
 
     public String timeToEnd() {
 
- /*       int numMinutes = 0;
+       
+        LocalTime localTime= new LocalTime();
+        LocalTime endTime = new LocalTime("16:00");
 
-        String currentTime = textTime.getText().toString();
-        String endingLesson = "05:00";
+// period of 1 year and 7 days
+        Period period = new Period(localTime, endTime);
 
-        try {
+// calc will equal end
+       // DateTime calc = start.plus(period);
 
-            SimpleDateFormat formatYmd = new SimpleDateFormat("HH:mm");
+// able to calculate whole days between two dates easily
+       // Days days = Days.daysBetween(start, end);
 
-            Date d1 = formatYmd.parse(currentTime);
-            Date d2 = formatYmd.parse(endingLesson);
+// able to calculate whole months between two dates easily
+        //Months months = Months.monthsBetween(start, end);
 
-            DateTime dt1 = new DateTime(d1);
-            DateTime dt2 = new DateTime(d2);
-            numMinutes = Minutes.minutesBetween(dt1, dt2).getMinutes();
-            // Days between 2013-09-01 and 2013-09-02: 1
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return String.valueOf(numMinutes);*/
-
-        DateTime now = DateTime.now();
-        DateTime dateTime = now.plusMinutes(10);
-        Seconds seconds = Seconds.secondsBetween(now, dateTime);
-
-        return String.valueOf(seconds);
+        //textViewDifference.setText(String.valueOf(period));
+        return String.valueOf(period.getMinutes());
     }
-
-   /* public long timeToEnd()  {
-
-
-
-
-        String string1 = "05:00:00 PM";
-        Date time1=null;
-        try {
-            time1 = new SimpleDateFormat("HH:mm a").parse(string1);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Calendar calendar1 = Calendar.getInstance();
-
-        calendar1.setTime(time1);
-
-        String string2 = "03:00:00 PM";
-        Date time2 = null;
-        try {
-            time2 = new SimpleDateFormat("HH:mm a").parse(string2);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.setTime(time2);
-        calendar2.add(Calendar.DATE, 1);
-
-      *//*  Date x = calendar1.getTime();
-        Date xy = calendar2.getTime();
-        long diff = x.getTime() - xy.getTime();
-        long diffMinutes = diff / (60 * 1000);
-        float diffHours = diffMinutes / 60;
-        System.out.println("diff hours" + diffHours);*//*
-
-        if(calendar2.get(Calendar.AM_PM) == 1 && calendar1.get(Calendar.AM_PM) == 0)     {
-            calendar2.add(Calendar.DATE, 1);
-        }
-        long diff = calendar1.getTimeInMillis() - calendar2.getTimeInMillis();
-
-
-
-        return (long) diff;
-    }*/
 }
+
+
 
